8fc06d3fa61aad373a7e0b4d6c89cf582270da64
andres gonzalez <andrespino2000@gmail.com>
2022-10-20 22:04:26 -0500

    v1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://pipe2000-techs.github.io/react-api-rick-and-morty/",
   plugins: [react()]
 })
